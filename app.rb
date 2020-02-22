716155c0f849945bc159dfcab099a2d34b38e25c
kcleckey <kcleckey@gmail.com>
2020-02-22 22:44:04 +0000

    news

--- app.rb
+++ app.rb
@@ -7,12 +7,15 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "eff06cd898db4a557b22d6ab451f9614"
 
 get "/" do
-  # show a view that asks for the location
+    view "geocode"
 end
 
 get "/news" do
-  # do everything else
+    results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates # => [lat, long]
+    @lat = "#{lat_long[0]}"
+    @long = "#{lat_long[1]}"
 end
